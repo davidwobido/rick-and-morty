904bc7fcb4a9120665c0e2061bceed9a004a4954
davidwobido <davidwobido@googlemail.com>
2021-10-20 12:05:30 +0200

    ♻ Change static character to mapped arrays with arrow function

--- main.js
+++ main.js
@@ -18,34 +18,43 @@ function renderApp() {
     ]
   );
 
+  const characters = [
+    {
+      pic: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
+      name: "Toxic Rick",
+      status: "Dead",
+      location: "Earth",
+      firstSeen: "Rest and Ricklaxation",
+    },
+    {
+      pic: "https://rickandmortyapi.com/api/character/avatar/199.jpeg",
+      name: "Larva Alien",
+      status: "Alive",
+      location: "Planet Squanch",
+      firstSeen: "Get Schwifty",
+    },
+    {
+      pic: "https://rickandmortyapi.com/api/character/avatar/219.jpeg",
+      name: "Mechanical Summer",
+      status: "Unknown",
+      location: "Earth",
+      firstSeen: "Rickmancing the Stone",
+    },
+  ];
+
+  const characterCards = characters.map((character) =>
+    createCharacterCard(character)
+  );
+
+  // const characterCards = function.map
+  //   return createCharacterCard(character);
+
   const mainElement = createElement(
     "main",
     {
       className: "main",
     },
-    [
-      createCharacterCard({
-        pic: "https://rickandmortyapi.com/api/character/avatar/361.jpeg",
-        name: "Toxic Rick",
-        status: "Dead",
-        location: "Earth",
-        firstSeen: "Rest and Ricklaxation",
-      }),
-      createCharacterCard({
-        pic: "https://rickandmortyapi.com/api/character/avatar/199.jpeg",
-        name: "Larva Alien",
-        status: "Alive",
-        location: "Planet Squanch",
-        firstSeen: "Get Schwifty",
-      }),
-      createCharacterCard({
-        pic: "https://rickandmortyapi.com/api/character/avatar/219.jpeg",
-        name: "Mechanical Summer",
-        status: "Unknown",
-        location: "Earth",
-        firstSeen: "Rickmancing the Stone",
-      }),
-    ]
+    characterCards
   );
 
   const footerElement = createElement("footer", {
